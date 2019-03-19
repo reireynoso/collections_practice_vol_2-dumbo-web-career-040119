251a83bfdcc7f128624f74372dad7d8b4aa8bcbc
Reinald Reynoso <reireynoso@gmail.com>
2019-03-19 03:27:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -78,7 +78,7 @@ def organize_schools(schools)
   organize = {}
   schools.each do |key,value|
     value.each do |location,city|
-      organize[city] = location
+      organize[city] = value
     end
   end
   puts organize
